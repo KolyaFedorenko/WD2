e2987013e3b054fdb746085d95e8ea8ee7026ad5
KolyaFedorenko <kolyafedorenko645@gmail.com>
2021-12-06 21:22:22 +0700

    added additional functions

--- WD.py
+++ WD.py
@@ -26,6 +26,9 @@ def messagesend(botmessage):
 def weather_message():
     vk.messages.send(user_id=event.user_id, message=str(text), random_id=get_random_id())
 
+def adminchatsend(chatmessage):
+    vk.messages.send(chat_id=3, message=chatmessage, random_id=get_random_id())
+
 def select_sql():
     messagesend('Введите SELECT-запрос')
     for event in longpoll.listen():
@@ -74,7 +77,7 @@ def generate_token(lenght):
     messagesend('Введите токен')
     symbols = string.ascii_letters + string.digits
     token = ''.join(secrets.choice(symbols) for i in range (lenght))
-    vk.messages.send(chat_id=3, message=token, random_id=get_random_id())
+    adminchatsend(token)
     for event in longpoll.listen():
       if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
           if event.text != '/back':
@@ -85,6 +88,56 @@ def generate_token(lenght):
           else:
             messagesend('Список возможностей:\na) Получить текущий прогноз погоды\nb) Получить прогноз погоды за прошедшую неделю\nc) Получать прогноз погоды по расписанию')
             return (0)
+
+def add_user():
+    getuser = vk.users.get(user_id=event.user_id, fields='')
+    getuser= ''.join(str(getuser) for getuser in getuser)
+    getuser = list(filter(str.isdigit, getuser))
+    getuser=str(getuser).replace("', '", "").replace("['", "").replace("']","")
+
+    check_id=cursor.execute("SELECT User_ID from users")
+    check_id=cursor.fetchall()
+    check_id= ''.join(str(check_id) for check_id in check_id)
+    check_id=check_id.replace("[(", "").replace(")]","").replace(",,",", ").replace("(","").replace(")","").replace(","," ")
+    check_id="" + getuser + "" in check_id
+    check_id=str(check_id)
+    if check_id == 'True':
+        adminchatsend('Пользователь с ID "' + getuser + '" не добавлен, так как уже существует в базе')
+    if check_id == 'False':
+        try:
+            cursor.execute("SELECT ID FROM Users WHERE ID=(SELECT MAX(ID) FROM Users)")
+            userid=cursor.fetchone()
+            userid= ''.join(str(userid) for userid in userid)
+            userid=int(userid)+1
+            add_user=cursor.execute("INSERT INTO Users (ID, User_ID, Role) VALUES ('" + str(userid) + "', '" + getuser + "', 'User');")
+            sqlite_connection.commit()
+            adminchatsend('Добавлен новый пользователь с ID "' + getuser + '"')
+        except Exception:
+            adminchatsend('При добавлении пользователя с ID "' + getuser + '" возникла ошибка')
+
+def update_db():
+    url = 'https://rp5.ru/Погода_в_Абакане'
+    response = requests.get(url)
+    print(response.status_code)
+    soup = BeautifulSoup(response.text, 'lxml')
+    forecasts = soup.find_all('div', class_='round-5')
+    for forecast in forecasts:
+        text=(forecast.text)
+        text=text.replace("Сегодня ", "По Цельсию сегодня ").replace("ожидается ", "ожидается от ").replace("..", "° до ").replace("°C°F","°").replace(" °, ","°, ")
+        text=text.replace("Завтра: ", "Завтра: по Цельсию от ").replace("°, +", "°, по Фаренгейту от +").replace("°, -", "°, по Фаренгейту от -")
+
+    try:
+        insertid=cursor.execute("SELECT ID FROM Forecasts WHERE ID=(SELECT MAX(ID) FROM Forecasts)")
+        insertid=cursor.fetchone()
+        insertid= ''.join(str(insertid) for insertid in insertid)
+        insertid=int(insertid)+1
+        messagesend(str(insertid))
+        text=text.replace("По Цельсию сегодня ожидается", "Ожидается").replace("Завтра:", "На следующий день:")
+        insertintotable=cursor.execute("INSERT INTO Forecasts (ID, Date, Forecast) VALUES ('" + str(insertid) + "', '" + str(datetime.date.today()) + "', '" + str(text) + "');")
+        sqlite_connection.commit()
+        adminchatsend('База была обновлена ' + str(datetime.date.today()) + ' в ' + str(datetime.datetime.now().time().strftime('%H:%M')))
+    except Exception:
+        adminchatsend('Возникла ошибка при обновлении базы ' + str(datetime.date.today()) + ' в ' + str(datetime.datetime.now().time().strftime('%H:%M')))
 
 def shedule_every_minutes():
     messagesend('Прогноз погоды будет приходить каждые полчаса')
@@ -135,6 +188,7 @@ for event in longpoll.listen():
             everyday_thread=threading.Thread(target=shedule_every_day).start()
 
         if event.text == 'привет' or event.text == 'начать' or event.text == 'menu' or event.text == 'меню' or event.text == 'назад':
+            add_user()
             messagesend('Список возможностей:\na) Получить текущий прогноз погоды\nb) Получить прогноз погоды за прошедшую неделю\nc) Получать прогноз погоды по расписанию')
         
         if event.text == 'a':
@@ -144,23 +198,18 @@ for event in longpoll.listen():
             date=cursor.execute("SELECT Date, Forecast FROM Forecasts")
             date=cursor.fetchall()
             datenew= ''.join(str(date) for date in date)
-            #datenew=datenew.replace("[", "").replace("]", "").replace("'", "").replace("(", "").replace(")", "").replace(",,", ", ").replace(",", ", ")
-            #datenew=datenew.replace("-", "/").replace("от /", "от -").replace("до /", "до -").replace(",", ": ").replace("°:","°,").replace("2021", "\n\n2021")
             datenew=datenew.replace("(","\n").replace(", '",": ").replace("'","").replace(")","")
             messagesend(datenew)
 
         if event.text == 'c':
             messagesend('Когда должен приходить прогноз погоды?\n\n1. Каждые полчаса\n2. Каждый час\n3. Каждые 3 часа\n4. Каждые 6 часов\n5. Утром и вечером')
 
-        #for everyday update
-            #insertid=cursor.execute("SELECT ID FROM Forecasts WHERE ID=(SELECT MAX(ID) FROM Forecasts)")
-            #insertid=cursor.fetchone()
-            #insertid= ''.join(str(insertid) for insertid in insertid)
-            #insertid=int(insertid)+1
-            #messagesend(str(insertid))
-            #text=text.replace("По Цельсию сегодня ожидается", "Ожидается").replace("Завтра:", "На следующий день:")
-            #insertintotable=cursor.execute("INSERT INTO Forecasts (ID, Date, Forecast) VALUES ('" + str(insertid) + "', '" + str(datetime.date.today()) + "', '" + str(text) + "');")
-            #sqlite_connection.commit()
-
         if event.text == '/admin':
-            generate_token(15)       
+            generate_token(30)
+
+        if event.text == 'get': # get user_id test
+            add_user()
+
+        if event.text == 'up': # update db test
+            update_db()
+
